06df03e1a190ba8ac0da7718031cc013cb1a79e4
mmyers <mmyers@users.noreply.github.com>
2023-02-11 22:06:35 -0600

    Implement conversion and user interface

--- src/provconverter/Main.java
+++ src/provconverter/Main.java
@@ -5,29 +5,112 @@ import eug.parser.EUGFileIO;
 import eug.shared.GenericObject;
 import java.io.File;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Michael
  */
 public class Main {
+    
+    private static String inputFilename;
+    private static String outputFilename;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         GenericObject config = EUGFileIO.load("mapping.txt");
-        GenericObject mappings = config.getChild("mappings");
+        if (config == null) {
+            JOptionPane.showMessageDialog(null, "Failed to load mappings from mapping.txt.", "Fatal error", JOptionPane.ERROR_MESSAGE);
+            System.exit(1);
+        }
         
-//        JFileChooser chooser = new JFileChooser();
-//        int result = chooser.showDialog(null, null);
-//        if (result == JFileChooser.APPROVE_OPTION) {
-//            File f = chooser.getSelectedFile();
-//        }
+        handleArgs(args);
+        
+        if (inputFilename == null || !(new File(inputFilename).exists())) {
+            JFileChooser chooser = new JFileChooser();
+            chooser.setDialogTitle("Select source file (province.csv or provinces.txt)");
+            int result = chooser.showOpenDialog(null);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                File f = chooser.getSelectedFile();
+                inputFilename = f.getAbsolutePath();
+            } else {
+                return;
+            }
+        }
+        
+        if (outputFilename == null || !(new File(outputFilename).exists())) {
+            JFileChooser chooser = new JFileChooser();
+            chooser.setDialogTitle("Select output file (provinces.txt or province.csv)");
+            int result = chooser.showOpenDialog(null);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                File f = chooser.getSelectedFile();
+                outputFilename = f.getAbsolutePath();
+            } else {
+                return;
+            }
+        }
 
-        ProvinceData data = new ProvinceData("eu2_province.csv", mappings);
-        data.saveTxt("eu2_provinces.txt");
+        System.out.println("Loading provinces...");
+        ProvinceData data = new ProvinceData(inputFilename, config);
+        
+        System.out.println("Saving converted provinces...");
+        if (inputFilename.endsWith(".csv"))
+            data.saveTxt(outputFilename);
+        else
+            data.saveCsv(outputFilename);
+        System.out.println("Done.");
+    }
 
+    private static void handleArgs(final String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            if (equals(arg, "-h", "--help")) {
+                printHelp();
+            } else if (arg.equals("-input")) {
+                inputFilename = stripQuotes(args[++i]);
+            } else if (arg.equals("-output")) {
+                outputFilename = stripQuotes(args[++i]);
+            } else {
+                System.err.println("Not a valid option: " + arg);
+                printHelp();
+            }
+        }
     }
     
+    private static boolean equals(String arg, String shortArg, String longArg) {
+        return (arg.equals(shortArg) || arg.equalsIgnoreCase(longArg));
+    }
+    
+    private static String stripQuotes(String str) {
+        if (str.startsWith("\"")) {
+            str = str.substring(1);
+        }
+        if (str.endsWith("\"")) {
+            str = str.substring(0, str.length() - 1);
+        }
+        return str;
+    }
+    
+    private static void printHelp() {
+        System.out.println("Usage: java -jar FTG_Province_Converter.jar [-args...]");
+        System.out.println();
+        System.out.println("where args include:");
+        System.out.println("    -input <filename>");
+        System.out.println("        The name of the input file to be converted.");
+        System.out.println("    -output <filename>");
+        System.out.println("        The name of the file where the converted output should be saved.");
+        System.out.println("    -h | --help");
+        System.out.println("        Print this help.");
+        System.out.println();
+        System.out.println("Note that if either -input or -output is not present, a file chooser will be shown.");
+        System.out.println();
+        System.out.println("If the input file ends with .csv, the output file will be assumed to be .txt, and vice versa.");
+        System.out.println();
+        System.out.println("Example:");
+        System.out.println("java -jar FTG_Province_Converter.jar -input \"D:\\games\\mynewmapmod\\db\\map\\provinces.txt\" -output \"D:\\games\\mynewmapmod\\db\\map\\province.csv\"");
+    }
+    
+    private Main() { }
 }
--- src/provconverter/ProvinceData.java
+++ src/provconverter/ProvinceData.java
@@ -30,21 +30,21 @@ import java.util.regex.Pattern;
  */
 public class ProvinceData {
     
-    public static final int NUM_PROVINCES = 2020;
-    
     private final Map<Integer, Province> allProvs = new HashMap<>();
-//    private final List<Province> extras = new ArrayList<Province>();
     
     private static final Pattern SEMICOLON = Pattern.compile(";");
     
-    private String headerString;
-    
-    private GenericObject provincesObj; // if FTG-style provinces.txt rather than CSV
+    private String headerString
+            = "Id;Name;PIW name;Religion;Culture;SizeModifier;Pashas;Climate;Ice?;Storm?;Galleys;Manpower;Income;Terrain;ToT-SPA;ToT-POR;HRE;Mine(1) ?;MineValue;Goods;Upgradable;CoT Historical Modifier;Difficulty for Colonization;Native Combat Strength;Ferocity;Efficiency of Natives in combat;Negotiation Value for Trading Posts;Natives Tolerance value ;City XPos;City YPos;Army XPos;ArmyYPos;PortXPos;Port YPos;Manufactory XPos; Manufactory YPos;Port/sea Adjacency;Terrain x;Terrain Y;Terrain variant;Terrain x;Terrain Y;Terrain variant;Terrain x;Terrain Y;Terrain variant;Terrain x;Terrain Y;Terrain variant;Area;Region;Continent;;Extra River Desc Link 1;Extra River Desc Link 2;Extra River Desc Link 3;;Fill coord X;Fill coord Y;;;;;;;;;;;;;;;;";
     
     private final GenericObject mappings;
+    private final GenericObject defaultMappings;
+    private final GenericObject conversions;
     
-    public ProvinceData(String filename, GenericObject mappings) {
-        this.mappings = mappings;
+    public ProvinceData(String filename, GenericObject config) {
+        this.mappings = config.getChild("mappings");
+        this.defaultMappings = config.getChild("defaults");
+        this.conversions = config.getChild("convert");
         
         if (filename.endsWith(".csv"))
             loadCsv(filename);
@@ -55,7 +55,7 @@ public class ProvinceData {
     private void loadCsv(String filename) {
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String currLine;
-            // Id;Name;PIW name;Religion;Culture;SizeModifier;Pashas;Climate;Ice?;Storm?;Galleys;Manpower;Income;Terrain;Mine(1) ?;MineValue;Goods;Upgradable;CoT Historical Modifier;Difficulty for Colonization;Native Combat Strength;Ferocity;Efficiency of Natives in combat;Negotiation Value for Trading Posts;Natives Tolerance value ;City XPos;City YPos;Army XPos;ArmyYPos;PortXPos;Port YPos;Manufactory XPos; Manufactory YPos;Port/sea Adjacency;Terrain x;Terrain Y;Terrain variant;Terrain x;Terrain Y;Terrain variant;Terrain x;Terrain Y;Terrain variant;Terrain x;Terrain Y;Terrain variant;Area;Region;Continent;;Extra River Desc Link 1;Extra River Desc Link 2;Extra River Desc Link 3;;Fill coord X;Fill coord Y;;;;;;;;;;;;;;;;
+            // Id;Name;PIW name;Religion;Culture;SizeModifier;Pashas;Climate;Ice?;Storm?;Galleys;Manpower;Income;Terrain;ToT-SPA;ToT-POR;HRE;Mine(1) ?;MineValue;Goods;Upgradable;CoT Historical Modifier;Difficulty for Colonization;Native Combat Strength;Ferocity;Efficiency of Natives in combat;Negotiation Value for Trading Posts;Natives Tolerance value ;City XPos;City YPos;Army XPos;ArmyYPos;PortXPos;Port YPos;Manufactory XPos; Manufactory YPos;Port/sea Adjacency;Terrain x;Terrain Y;Terrain variant;Terrain x;Terrain Y;Terrain variant;Terrain x;Terrain Y;Terrain variant;Terrain x;Terrain Y;Terrain variant;Area;Region;Continent;;Extra River Desc Link 1;Extra River Desc Link 2;Extra River Desc Link 3;;Fill coord X;Fill coord Y;;;;;;;;;;;;;;;;
             // 380;Flandern;coastal;reformed;dutch;0;0;3;0;0;0;6;17;0;0;0;clo;1;4;0;0;0;0;0;0;9505;1636;9548;1671;9475;1615;9502;1677;938;-100;-100;0;-100;-100;0;-100;-100;0;-100;-100;0;Low Countries;Western Europe;Europe;Antwerpen;0;0;0;1;9522;1649;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1
             
             int id = -1;
@@ -76,26 +76,19 @@ public class ProvinceData {
                         
                         if (id >= 0) {
                             allProvs.put(id, new ProvinceCsv(args));
-                        } /*else {
-                            extras.add(new Province(args));
-                        }*/
-                    } /*else {
-                        extras.add(new Province(args));
-                    }*/
+                        }
+                    }
                 } catch (RuntimeException e) {
-                    System.err.print("Error with "+id+": ");
-                    e.printStackTrace();
+                    Logger.getLogger(ProvinceData.class.getName()).log(Level.SEVERE, "Error with " + id, e);
                 }
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
-            e.printStackTrace();
+            Logger.getLogger(ProvinceData.class.getName()).log(Level.SEVERE, null, e);
         }
     }
     
     private void loadTxt(String filename) {
-        provincesObj = EUGFileIO.load(filename);
+        GenericObject provincesObj = EUGFileIO.load(filename);
         
         for (GenericObject prov : provincesObj.getChildren("province")) {
             String idStr = prov.getString("id");
@@ -105,9 +98,7 @@ public class ProvinceData {
     }
     
     public void saveCsv(String filename) {
-        BufferedWriter writer = null;
-        try {
-            writer = new BufferedWriter(new FileWriter(filename));
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
             
             if (headerString != null) {
                 writer.write(headerString);
@@ -115,7 +106,8 @@ public class ProvinceData {
             }
 
             boolean writtenInvalid = false;
-            int maxProv = allProvs.keySet().stream().max(Comparator.naturalOrder()).get();
+            int maxProv = allProvs.keySet().stream().max(Comparator.naturalOrder()).orElse(0);
+            int numMappings = mappings.values.stream().mapToInt(v -> Integer.parseInt(v.varname)).max().getAsInt();
             for (int i = 0; i <= maxProv; i++) {
                 Province p = getProvince(i);
                 if (p == null) {
@@ -126,19 +118,19 @@ public class ProvinceData {
                         writer.write(";;;;;;;;;;;;;;0;0;0;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;");
                     }
                 } else {
-                    p.writeOut(writer);
+                    String[] entry = new String[numMappings];
+                    convertTxtProvince((ProvinceTxt)p, entry);
+                    
+                    for (int col = 0; col < entry.length-1; col++) {
+                        writer.write(entry[col]);
+                        writer.write(';');
+                    }
+                    writer.write(entry[entry.length-1]);
                 }
                 writer.newLine();
             }
         } catch (IOException ex) {
             Logger.getLogger(ProvinceData.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-                if (writer != null)
-                    writer.close();
-            } catch (IOException ex) {
-                Logger.getLogger(ProvinceData.class.getName()).log(Level.SEVERE, null, ex);
-            }
         }
     }
     
@@ -200,26 +192,61 @@ public class ProvinceData {
             curr.setString(s[s.length-1], value);
     }
     
+    private void convertTxtProvince(ProvinceTxt p, String[] entry) {
+        for (int i = 0; i < entry.length; i++) {
+            String colStr = Integer.toString(i);
+            String mapping = mappings.getString(colStr);
+            if (!mapping.isEmpty() && !mapping.equals("unused")) {
+                entry[i] = normalizeAndGet(p.provObj, mapping);
+            } 
+            if (entry[i] == null || entry[i].isEmpty()) {
+                entry[i] = defaultMappings.getString(colStr); // will be "" if no default exists
+            } else if (entry[i].equals("yes")) {
+                entry[i] = "1";
+            } else if (entry[i].equals("no")) {
+                entry[i] = "0";
+            } else if (conversions.containsChild(colStr)) {
+                GenericObject convert = conversions.getChild(colStr);
+                if (convert.contains(entry[i])) {
+                    entry[i] = convert.getString(entry[i]);
+                }
+            }
+        }
+    }
+    
+    
+    private String normalizeAndGet(GenericObject parent, String path) {
+        String[] s = path.split("\\.");
+        GenericObject curr = parent;
+        
+        // iterate until we have only the last piece left
+        for (int i = 0; i < s.length - 1; ++i) {
+            if (s[i+1].equals("list")) {
+                // if the next piece is "list", create a list instead of an object
+                // then add the value to the list and we're done
+                GenericList tmp = curr.getList(s[i]);
+                if (tmp == null)
+                    return "";
+                
+                int index = Integer.parseInt(s[i+2]); // TODO: will crash if mapping doesn't have index
+                return tmp.get(index);
+            } else {
+                GenericObject tmp = curr.getChild(s[i]);
+                if (tmp == null) {
+                    tmp = curr.createChild(s[i]);
+                }
+                curr = tmp;
+            }
+        }
+        
+        return curr.getString(s[s.length-1]);
+    }
+    
     public Province getProvince(int id) {
         return allProvs.get(id);
     }
     
-    public boolean isPTI(int id) {
-        final Province p = getProvince(id);
-        return p == null;
-    }
-    
-    public String getName(int id) {
-        final Province p = getProvince(id);
-        if (p == null)
-            return "Terra Incognita";
-        
-        return p.getName();
-    }
-    
     public interface Province {
-        
-        String getName();
         
         void writeOut(BufferedWriter out) throws IOException;
     }
@@ -227,37 +254,8 @@ public class ProvinceData {
     private static class ProvinceCsv implements Province {
         private final String[] entry;
         
-        public static final int NAME_IDX = 1;
-        public static final int TERRAIN_IDX = 13;
-        // NOTE: Entries 14, 15, and 16 were added in 1.09
-        public static final int CITY_IDX = 28;
-        public static final int ARMY_IDX = 30;
-        public static final int PORT_IDX = 32;
-        public static final int MANU_IDX = 34;
-        public static final int TERRAIN_1_IDX = 37;
-        public static final int TERRAIN_1_TYPE_IDX = 39;
-        public static final int TERRAIN_2_IDX = 40;
-        public static final int TERRAIN_2_TYPE_IDX = 42;
-        public static final int TERRAIN_3_IDX = 43;
-        public static final int TERRAIN_3_TYPE_IDX = 45;
-        public static final int TERRAIN_4_IDX = 46;
-        public static final int TERRAIN_4_TYPE_IDX = 48;
-        
         private ProvinceCsv(String[] entry) {
             this.entry = entry;
-        }
-        
-        public String getString(int idx) {
-            return entry[idx];
-        }
-        
-        public int getInt(int idx) {
-            return Integer.parseInt(entry[idx]);
-        }
-        
-        @Override
-        public String getName() {
-            return entry[NAME_IDX];
         }
         
         @Override
@@ -272,16 +270,9 @@ public class ProvinceData {
     
     private static final class ProvinceTxt implements Province {
         private final GenericObject provObj;
-        private final String name;
         
         ProvinceTxt(GenericObject obj) {
             this.provObj = obj;
-            this.name = obj.getString("name");
-        }
-
-        @Override
-        public String getName() {
-            return name;
         }
 
         @Override
